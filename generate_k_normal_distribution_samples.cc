837791cc69c83255dda2bca831907a2ac5c99d1e
enigmadimitri <enigmadimitri@gmail.com>
2024-10-22 11:46:05 +0200

    [FEAT] Script generating k samples from a normal distribution (#1)

---
+++ generate_k_normal_distribution_samples.cc
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <string>
+#include <sstream>
+
+int main()
+{
+    int k;
+    std::random_device random_device;
+    std::mt19937 generator{random_device()};
+    std::normal_distribution<double> normal_distribution;
+
+    std::cout << "Enter the value of k:" << std::endl;
+    std::cin >> k;
+    
+    std::string filename = "data/samples/" + std::to_string(k) + "_normal_distribution_samples.data";
+    std::ofstream file(filename);
+    for(int i = 0; i < k - 1; i++){
+        file << normal_distribution(generator) << std::endl;
+    }
+    file << normal_distribution(generator);
+    file.close();
+    return 0;
+}
